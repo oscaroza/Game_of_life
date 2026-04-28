b91f7921affa4c48a16d6d883266bbf3c21ee83e
oscaroaz <oscarzech1@icloud.com>
2026-04-28 11:11:02 +0200

    supression de choses inutiles

--- launch_game.py
+++ launch_game.py
@@ -1,12 +1,14 @@
 from welcome import show_welcome
 
+show_welcome(duration_seconds=3)
+import UI
 
-def main():
-    should_start_game = show_welcome(duration_seconds=3)
-    if not should_start_game:
-        return
-    import UI
+##def main():
+    ##should_start_game = show_welcome(duration_seconds=3)
+    ##if not should_start_game:
+        ##return
+    ##import UI
 
 
-if __name__ == "__main__":
-    main()
+##if __name__ == "__main__":
+##main()
